e1350a42d2147477e6c82a2370f05a7f58957295
richieyuyongpoh <86595942+richieyuyongpoh@users.noreply.github.com>
2021-07-02 19:30:03 +0800

    Update AI4Fun.py

--- AI4Fun.py
+++ AI4Fun.py
@@ -90,7 +90,7 @@ if st.sidebar.button('RUN'):
         if r.json().get('output'):
            st.write("AI suggested description:")
            st.write('')
-           st.write(r.json()['output']) 
+           st.write("<font color='Aquamarine'> + r.json()['output']+</font>", unsafe_allow_html=True) 
            st.write('')
            st.write('')
            st.write('Original Image')
