bfd6eb00d0fc3ef8d3281299113c893893c13eba
richieyuyongpoh <86595942+richieyuyongpoh@users.noreply.github.com>
2021-07-12 16:51:30 +0800

    Update AI4Fun.py

--- AI4Fun.py
+++ AI4Fun.py
@@ -6,6 +6,15 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            footer:after {
+	content:'goodbye'; 
+	visibility: visible;
+	display: block;
+	position: relative;
+	#background-color: red;
+	padding: 5px;
+	top: 2px;
+}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
