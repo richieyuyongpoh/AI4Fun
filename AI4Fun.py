b695aeedd7895ebc890bd31f2899991e2a2c5e0a
richieyuyongpoh <86595942+richieyuyongpoh@users.noreply.github.com>
2021-07-12 16:49:33 +0800

    Update AI4Fun.py

--- AI4Fun.py
+++ AI4Fun.py
@@ -1,6 +1,16 @@
 import streamlit as st 
 import requests 
 from tempfile import NamedTemporaryFile
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
  
 st.title('Demo: AI for Fun. ')
 
