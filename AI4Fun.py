c881affceb6e27c914e93f03c85fea258aa50d4f
richieyuyongpoh <86595942+richieyuyongpoh@users.noreply.github.com>
2021-07-12 16:52:11 +0800

    Update AI4Fun.py

--- AI4Fun.py
+++ AI4Fun.py
@@ -5,16 +5,15 @@ from tempfile import NamedTemporaryFile
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
             footer:after {
-	content:'goodbye'; 
-	visibility: visible;
-	display: block;
-	position: relative;
-	#background-color: red;
-	padding: 5px;
-	top: 2px;
-}
+		content:'goodbye'; 
+		visibility: visible;
+		display: block;
+		position: relative;
+		#background-color: red;
+		padding: 5px;
+		top: 2px;
+		}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
